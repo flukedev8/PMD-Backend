6c88193dd731f4173148e8dadb9a29876e6d1fff
flukedev8 <sittisak.951753@gmail.com>
2021-07-16 15:56:48 +0700

    Setup Express js

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const books = require('../Database/db.json')
+
+router.get('/',(req, res) =>{
+    res.send('Hello APIs Welcome');
+});
+
+
+router.get('/books', (req, res) => {
+  res.json(books)
+})
+
+router.get('/books/:id', (req, res) => {
+    res.json(books.find(book => book.id === req.params.id))
+})
+
+router.post('/books', (req, res) => {
+    const name = req.body
+    const keep_name = "Name is : "+name
+    books.push(req.body)
+    res.status(201).json(req.body)
+})
+
+router.post('/setup-profile', (req, res) => {
+    books.push(req.body)
+    res.status(201).json(req.body)
+})
+
+router.put('/books/:id', (req, res) => {
+    const updateIndex = books.findIndex(book => book.id === req.params.id)
+    res.json(Object.assign(books[updateIndex], req.body))
+  })
+
+router.delete('/books/:id', (req, res) => {
+    const deletedIndex = books.findIndex(book => book.id === req.params.id)
+    books.splice(deletedIndex, 1)
+    res.status(204).send()
+ })
+
+module.exports = router;
